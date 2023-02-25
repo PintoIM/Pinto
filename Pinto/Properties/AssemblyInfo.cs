e4887c3985c30f171426c248d6c5ee855d96b012
vlOd <vlodiscord@gmail.com>
2023-02-26 01:24:55 +0200

    Rebrand to Pinto from PintoChat

--- Pinto/Properties/AssemblyInfo.cs
+++ Pinto/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PintoChat")]
-[assembly: AssemblyDescription("A simple IM client")]
+[assembly: AssemblyTitle("Pinto")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PintoChat")]
+[assembly: AssemblyProduct("Pinto")]
 [assembly: AssemblyCopyright("vlOd")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
