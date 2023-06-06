4438fadb9011ddae95ce4a012bb695b67aa949cf
vlOd <vlodiscord@gmail.com>
2023-06-06 23:55:14 +0300

    Change to b1.0-pre1

--- Pinto/Program.cs
+++ Pinto/Program.cs
@@ -18,7 +18,7 @@ namespace PintoNS
     {
         // Constants
         public static ConsoleForm Console;
-        public const string VERSION_STRING = "b1.0";
+        public const string VERSION_STRING = "b1.0-pre1";
         public const int PROTOCOL_VERSION = 1;
 
         // Data paths
