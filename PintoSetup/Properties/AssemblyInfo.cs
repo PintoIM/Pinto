e56cb5c14f8b91c7fe939cfb926a39215bf95d3e
vlOd <66838724+vlOd2@users.noreply.github.com>
2024-01-18 22:09:39 +0200

    Change assembly information

--- PintoSetup/Properties/AssemblyInfo.cs
+++ PintoSetup/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PintoSetup")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The official installer for Pinto!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("PintoIM")]
 [assembly: AssemblyProduct("PintoSetup")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
