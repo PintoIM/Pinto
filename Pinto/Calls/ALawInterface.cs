a757d614f471007713760b24a08f9fff3908dd3c
vlOd2 <66838724+vlOd2@users.noreply.github.com>
2024-03-10 18:00:30 +0200

    Add audio testing

---
+++ Pinto/Calls/ALawInterface.cs
@@ -0,0 +1,38 @@
+﻿using NAudio.Codecs;
+using System;
+
+namespace PintoNS.Calls
+{
+    // Taken from https://github.com/naudio/NAudio/blob/master/NAudioDemo/NetworkChatDemo/ALawChatCodec.cs
+    public class ALawInterface
+    {
+        public int SampleRate = 16000;
+        public int BitsPerSecond => SampleRate * 8;
+
+        public byte[] Encode(byte[] data) => Encode(data, 0, data.Length);
+
+        public byte[] Decode(byte[] data) => Decode(data, 0, data.Length);
+
+        public byte[] Encode(byte[] data, int offset, int length)
+        {
+            byte[] encoded = new byte[length / 2];
+            int outIndex = 0;
+            for (int n = 0; n < length; n += 2)
+                encoded[outIndex++] = ALawEncoder.LinearToALawSample(BitConverter.ToInt16(data, offset + n));
+            return encoded;
+        }
+
+        public byte[] Decode(byte[] data, int offset, int length)
+        {
+            byte[] decoded = new byte[length * 2];
+            int outIndex = 0;
+            for (int n = 0; n < length; n++)
+            {
+                short decodedSample = ALawDecoder.ALawToLinearSample(data[n + offset]);
+                decoded[outIndex++] = (byte)(decodedSample & 0xFF);
+                decoded[outIndex++] = (byte)(decodedSample >> 8);
+            }
+            return decoded;
+        }
+    }
+}
---
+++ Pinto/Forms/IntAudioTestForm.cs
@@ -0,0 +1,55 @@
+﻿using PintoNS.Calls;
+using System;
+using System.Windows.Forms;
+
+namespace PintoNS.Forms
+{
+    public partial class IntAudioTestForm : Form
+    {
+        private AudioRecorder recorder;
+        private AudioPlayer player;
+        private ALawInterface codec = new ALawInterface();
+
+        public IntAudioTestForm()
+        {
+            InitializeComponent();
+            Icon = Program.GetFormIcon();
+        }
+
+        public void Start()
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            recorder = new AudioRecorder();
+            player = new AudioPlayer();
+
+            player.Start();
+            recorder.Start();
+            recorder.MicrophoneDataAvailable += Recorder_MicrophoneDataAvailable;
+        }
+
+        private void Recorder_MicrophoneDataAvailable(byte[] data)
+        {
+            byte[] encoded = codec.Encode(data, 0, data.Length);
+            Program.Console.WriteMessage($"{data.Length} -> {encoded.Length}");
+            player.Play(codec.Decode(encoded, 0, encoded.Length));
+        }
+
+        public void Stop()
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            if (recorder != null) recorder.Stop();
+            if (player != null) player.Stop();
+            recorder = null;
+            player = null;
+        }
+
+        private void IntAudioTestForm_FormClosing(object sender, FormClosingEventArgs e) => Stop();
+
+        private void btnStart_Click(object sender, EventArgs e) => Start();
+
+        private void btnStop_Click(object sender, EventArgs e) => Stop();
+    }
+}
